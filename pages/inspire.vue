bb7eeeb333b1729ec8a54a2b89e1f687d8e1b11e
lukbj623 <lukbj623@student.liu.se>
2023-09-27 08:48:32 +0200

    Simple express implemented

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -10,7 +10,24 @@
             </small>
           </footer>
         </blockquote>
+        <div></div>
+        <button @click="showMessageFromBackend">Show message from backend</button>
       </v-col>
     </v-row>
   </template>
-  
+
+
+<script>
+export default {
+  methods: {
+    async showMessageFromBackend(){
+      try{
+        const response = await this.$axios.get('/api/test')
+        console.log(response.data)
+      }catch (err){
+        console.log(err)
+      }
+    }
+  }
+}
+</script>
