b7d6c44cee83b26b1237b1f43326c0c6d481849c
Jon Karlsson <jonka268@student.liu.se>
2023-10-02 10:58:42 +0200

    Added rudimentary member list and member pages

---
+++ pages/admin/edit_offer_want.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+		<h1>Edit offer or want</h1>
+
+
+	</div>
+</template>
+
+
+
+<script>
+
+export default {
+
+	data() {
+		return {
+			offers:
+				[  // Fake offers
+					{ id: 1, offer: "water buffalo", price: 25 },
+					{ id: 2, offer: "moon rocket", price: 12 },
+					{ id: 3, offer: "heavy water", price: 50 },
+				],
+
+			wants:
+				[  // Fake wants
+					{ id: 1001, want: "spring", price: 25 },
+					{ id: 1002, want: "suspension", price: 12 },
+					{ id: 1003, want: "gearbox", price: 50 },
+				],
+
+
+			id: this.$route.params.id,
+		}
+
+	},
+
+
+
+}
+</script>
---
+++ pages/admin/offers_wants.vue
@@ -0,0 +1,67 @@
+<template>
+	<div>
+		<h1>Offer & Wants for {{ name }} </h1>
+		<br>
+		<h2>Offers</h2>
+		<ul>
+			<div v-for="offer in offers">
+				<li>
+					<!-- < NuxtLink : to = "{ name: 'admin-member', params: { id: member.id, name: member.name } }" > Member: {
+					{
+						member.name }}</NuxtLink> <a v-bind:href="`mailto:${member.email}`">Email</a> -->
+					{{ offer.offer }} - {{ offer.price }} <NuxtLink
+						:to="{ name: 'admin-edit_offer_want', params: { id: id } }">
+						<v-btn>Edit</v-btn>
+					</NuxtLink>
+				</li>
+			</div>
+		</ul>
+		<br>
+		<h2>Wants</h2>
+		<ul>
+			<div v-for="want in wants">
+				<li>
+					<!-- < NuxtLink : to = "{ name: 'admin-member', params: { id: member.id, name: member.name } }" > Member: {
+					{
+						member.name }}</NuxtLink> <a v-bind:href="`mailto:${member.email}`">Email</a> -->
+					{{ want.want }} - {{ want.price }}
+				</li>
+			</div>
+		</ul>
+
+	</div>
+</template>
+
+
+
+<script>
+
+export default {
+
+	data() {
+		return {
+			offers:
+				[  // Fake offers
+					{ id: 1, offer: "water buffalo", price: 25 },
+					{ id: 2, offer: "moon rocket", price: 12 },
+					{ id: 3, offer: "heavy water", price: 50 },
+				],
+
+			wants:
+				[  // Fake wants
+					{ id: 1001, want: "spring", price: 25 },
+					{ id: 1002, want: "suspension", price: 12 },
+					{ id: 1003, want: "gearbox", price: 50 },
+				],
+
+
+			id: this.$route.params.id,
+			name: this.$route.params.name
+		}
+
+	},
+
+
+
+}
+</script>
