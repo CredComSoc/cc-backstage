24acccc2ccc83b0184a7018c5304a76de9536065
Eugene Muthui <eugenemuthui@Eugenes-MacBook-Pro.local>
2024-08-01 14:46:07 +0300

    Hard-code env vars

--- api/config.js
+++ api/config.js
@@ -1,10 +1,10 @@
 // const CC_BACKEND_URL = process.env.CC_BACKEND_URL
 require('dotenv').config()
 const CC_BACKEND_URL = "https://api.credcomstaging.mutualcredit.services"
-const CC_NODE_URL = process.env.CC_NODE_URL
-const CC_NODE_ENABLED = process.env.CC_NODE_ENABLED
-const DB_URL = process.env.DB_URL
-const DB_FOLDER = process.env.DB_FOLDER
+const CC_NODE_URL = "http://ledger-staging.mutualcredit.services"
+const CC_NODE_ENABLED = true
+const DB_URL = "mongodb+srv://MCS-SB-admin:4%25D3SgU3x%24gCJUFCQI@lcc-dev.fwxxeyv.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
+const DB_FOLDER = "vt-web-app"
 
 console.log("Environment variables: ", CC_BACKEND_URL, CC_NODE_URL, DB_URL, DB_FOLDER, CC_NODE_ENABLED)
 
